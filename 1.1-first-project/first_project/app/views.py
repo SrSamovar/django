bd08771f8feb1cc885cf36ac221d0decb2766c24
Vadim Levchenko <levchenkovadim2001@mail.ru>
2024-08-24 02:29:47 +0300

    first project

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
+import os
 
 
 def home_view(request):
     template_name = 'app/home.html'
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
+
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,7 +26,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now().strftime('%H:%M:%S %d.%m.%Y')
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +35,5 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    workdir = os.listdir()
+    return HttpResponse('>'.join(workdir))
--- 1.1-first-project/first_project/first_project/wsgi.py
+++ 1.1-first-project/first_project/first_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for first_project project.
+WSGI config for first_project1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
